c716cabcb186f8745ad785cec4d9734d333055db
Vinicius Trindade <vinicius.ribeiro@ccc.ufcg.edu.br>
2021-10-16 11:58:55 -0300

    Datte manipulation with spring and database

---
+++ src/main/java/com/datas/aula5appspringdatas/model/Tarefas.java
@@ -0,0 +1,82 @@
+package com.datas.aula5appspringdatas.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
+
+@Entity
+public class Tarefas {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String tarefa;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Calendar createdAt;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate date;
+    
+    
+    @Column
+    private Date criadoEm;
+    
+    @PrePersist
+    protected void onCreate() {
+    	criadoEm = new Date();
+    }
+
+	
+    
+    
+    
+    
+    //getters e setters
+    public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTarefa() {
+		return tarefa;
+	}
+
+	public void setTarefa(String tarefa) {
+		this.tarefa = tarefa;
+	}
+
+	public Calendar getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Calendar createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public Date getCriadoEm() {
+		return criadoEm;
+	}
+
+	public void setCriadoEm(Date criadoEm) {
+		this.criadoEm = criadoEm;
+	}
+ 
+    
+}
